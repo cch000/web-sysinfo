845716f891ae720fdd48e7a0e236bd3f5ce0180b
Carlos <carloscamposherrera446@gmail.com>
2025-09-14 21:45:20 +0200

    improve err management

--- src/main.js
+++ src/main.js
@@ -6,27 +6,35 @@ const exec = promisify(require('child_process').exec)
 
 app.use(express.json());
 
+function pretty(n) {
+  return Number(n.toPrecision(4))
+}
+
 app.get('/status', async (req, res) => {
 
-  let output = exec('systemctl is-system-running').then(x => x.stdout).catch(
-    x => console.error("Error: " + x.stderr)
-  );
+  let output = await exec('systemctl is-system-running')
+    .then(x => x.stdout.trim())
+    .catch(y => (y.stdout || y.stderr).trim());
 
-  let availmem = (os.freemem() / (1024 * 1024 * 1024))
-    .toPrecision(4)
+  let availmem = pretty(os.freemem() / (1024 * 1024 * 1024))
 
-  let totalmem = (os.totalmem() / (1024 * 1024 * 1024))
-    .toPrecision(4)
+  let totalmem = pretty(os.totalmem() / (1024 * 1024 * 1024))
 
-  let usedmem = (totalmem - availmem).toPrecision(4)
+  let usedmem = pretty(totalmem - availmem)
 
-  res.json({
-    status: output,
-    availmem: Number(availmem),
-    totalmem: Number(totalmem),
-    usedmem: Number(usedmem),
-    loadvg: os.loadavg()
-  });
+
+  if (output == "running" || output == "degraded") {
+    res.json({
+      status: output,
+      availmem: availmem,
+      totalmem: totalmem,
+      usedmem: usedmem,
+      loadvg: os.loadavg()
+    });
+  } else {
+    console.error("Error: " + output)
+    res.status(500).send()
+  }
 });
 
 const PORT = 8114;
